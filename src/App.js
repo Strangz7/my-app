6e47ca642aeca831e231ec7d857aa80367ef0a30
Strangz7 <stephenadebayo79@gmail.com>
2022-08-04 14:44:27 +0100

    Added react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./views/authentication/Login";
 import "./App.css"
 import Register from "./views/authentication/Register";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Dashboard from "./views/dashboard/Dashboard";
 
 const App=()=>{
   return(
@@ -10,6 +11,7 @@ const App=()=>{
         <Routes>
           <Route path="/login" element={<Login/>}/>
           <Route path="/" element={<Register/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
       </Router>
   
