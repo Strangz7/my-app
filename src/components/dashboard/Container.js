55e3794fd181f348b40f1e9df0e8625d6d7a3f1e
Strangz7 <stephenadebayo79@gmail.com>
2022-08-05 16:17:55 +0100

    Added react files

--- src/components/dashboard/Container.js
+++ src/components/dashboard/Container.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import SideNav from './SideNav'
 
+
 const Container = (props) => {
   return (
     <>
--- src/components/dashboard/SideNav.js
+++ src/components/dashboard/SideNav.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsList } from "react-icons/bs";
+import {BsFillWalletFill} from "react-icons/bs";
 
 const SideNav = () => {
+  const [width, setWidth] = useState("20%")
+  const[activeLink, setActiveLink] = useState(null)
+  let activeStyle = {
+    backgroundColor: "#474747",
+    color: 'var(--primary_green)'
+  }
+
+  const links = ["events", "analytics", "marketing"]
+  
+
+  function toggleWidth(){
+    width === "20%" ? setWidth("10%") : setWidth("20%")
+  }
+
   return (
-    <div className='dashboardSideNav'>
-      <div className='sideNavLogoSection'>
+    <div className='dashboardSideNav' style={{ width: width }}>
+      <div className='sideNavLogoSection' >
+        <div id='sideNavLogo'>
+          N 
+        </div>
+        <div id='sideNavHamburger'>
+            <BsList color='white' display="flex" fontSize={22} onClick={toggleWidth} cursor='pointer'/>
+        </div>
       </div>
-      SideNav
+        <div className='sideNavLinks'>
+          <div className='links'>
+            {links.map((link, index) => 
+              <button className='link'
+                key={index}
+                onClick={() => setActiveLink(link)} 
+                style={ activeLink === link ? activeStyle: {color:'white' }}>
+                <BsFillWalletFill fontSize={20} 
+                  style={{ marginRight: "2rem" }}
+                  color={activeLink === link ? 'var(--primary_green)' : 'white' }/>
+                {link} 
+              </button>
+            )}
+          </div>
+        </div>
     </div>
   )
 }
